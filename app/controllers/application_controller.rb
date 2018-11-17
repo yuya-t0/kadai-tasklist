fe98a18510613d61514aa204681ab881608c4681
yuya.t <y.tamagawa123@gmail.com>
2018-11-17 07:11:16 +0000

    login3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,20 @@ class ApplicationController < ActionController::Base
   
   include SessionsHelper
 
+
   
   private
   
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
     end
   end
   
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,6 @@ class TasksController < ApplicationController
 
 
   def index
-    #@tasks = Task.order(id: :desc).page(params[:page])
   end
   
   def show
